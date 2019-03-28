b08adb2311d35f622581940f579907dec3b51279
Nobot__ <31627752+Yatheon@users.noreply.github.com>
2019-03-28 14:12:06 +0100

    Update App.java

--- sourcecode/src/main/java/com/toggle/api/App.java
+++ sourcecode/src/main/java/com/toggle/api/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello WorRRRRRRRRRRRld! Hello Michael !! I am daniel" );
+        System.out.println( "Hello WorRRRRRRRRRRRld! Hello Michael !! I am danielHello" );
     }
 }

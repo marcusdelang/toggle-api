c32f630bb95d053cda1a60c5133c2da8ca0a7aac
Marcus <mannils233@gmail.com>
2019-04-29 13:19:21 +0200

    added server-config file

--- toggle-api/index.js
+++ toggle-api/index.js
@@ -1,3 +1,10 @@
 var server = require('./server/server');
+var fs = require('fs');
 
-server.start()
+fs.readFile('server-config.json', function (err, data) {
+    if (err) {
+        return console.log('ERROR READING CONFIG FILE: ' + err);
+    }
+    var config = JSON.parse(data);
+    server.start(config);
+});
--- toggle-api/server/server.js
+++ toggle-api/server/server.js
@@ -2,24 +2,23 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var router = require('./router');
 
-function start() {
+function start(config) {
   var app = express();
 
-// Set up server configuration
-  
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+  // Set up server configuration
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+  // parse application/json
   app.use(bodyParser.json())
-  
-// Routing
-router(app);
-  
-var port = process.env.PORT || 3000
-app.listen(port, function() {
+
+  // Set up routes (endpoints)
+  router(app);
+
+  var port = process.env.PORT || config.port;
+  app.listen(port, function () {
     console.log("Server is running. If local, try http://localhost:" + port);
-});
+  });
 }
 
 module.exports.start = start;
